a4b1e27f76cea215bd5340a448a353e8904136e2
Daniel Hug <daniel@hugwebdesign.com>
2017-05-23 14:57:05 -0700

    Update user-custom.js

--- user-custom.js
+++ user-custom.js
@@ -23,23 +23,29 @@ var on = (function() {
 	};
 })();
 
+function makeRequest(url, cb) {
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+			cb(request.responseText);
+		}
+	};
+	request.open('GET', url);
+	request.send();
+}
+
+function loadScript(url, cb) {
+	var prefix = '(function(define) {';
+	var postfix = '})();';
+
+	makeRequest(url, function(js) {
+		var script = document.createElement('script');
+		script.text = prefix + js + postfix;
+		script.onload = cb;
+		document.body.appendChild(script);
+	});
+}
 
 loadScript('https://daniel-hug.github.io/stackedit-extensions/reftagger.js');
 loadScript('https://daniel-hug.github.io/stackedit-extensions/custom-css.js');
 loadScript('https://daniel-hug.github.io/stackedit-extensions/oeis-linker.js');
-
-function loadScript(url, cb) {
-	var s = document.createElement('script');
-	s.src = url;
-	s.defer = true;
-	var avoidRequireJS = typeof define === 'function' && define.amd;
-	if (avoidRequireJS) {
-		var tmp = define;
-		define = null;
-	}
-	s.onload = function() {
-		if (avoidRequireJS) define = tmp;
-		if (cb) cb();
-	};
-	document.body.appendChild(s);
-}
