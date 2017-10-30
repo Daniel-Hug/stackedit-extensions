1fefa6bded281cde26d22491dfac8fafc8673a82
Daniel Hug <daniel@hugwebdesign.com>
2017-10-30 15:35:12 -0700

    differentiate nested <mark> elements with a gamut of colors

--- custom-css.js
+++ custom-css.js
@@ -26,10 +26,52 @@ function embedStyle(fn) {
 on('Ready', function() {
 	embedStyle(function() {/*!
 	ul, ol, li {
-	  position: relative;
+		position: relative;
 	}
 	li {
-	  page-break-inside: avoid;
+		page-break-inside: avoid;
+	}
+	
+	/* differentiate nested <mark> elements with a gamut of colors */
+	mark {
+		color: #000;
+		transition: background-color 0.2s ease-in;
+	}
+
+	mark,
+	mark:hover,
+	mark:hover mark {
+		background: hsl(0, 100%, 90%);
+	}
+
+	mark > mark,
+	mark > mark:hover,
+	mark > mark:hover mark {
+		background: hsl(60, 100%, 85%);
+	}
+
+	mark > mark > mark,
+	mark > mark > mark:hover,
+	mark > mark > mark:hover mark {
+		background: hsl(120, 100%, 80%);
+	}
+
+	mark > mark > mark > mark,
+	mark > mark > mark > mark:hover,
+	mark > mark > mark > mark:hover mark {
+		background: hsl(180, 100%, 75%);
+	}
+
+	mark > mark > mark > mark > mark,
+	mark > mark > mark > mark > mark:hover,
+	mark > mark > mark > mark > mark:hover mark {
+		background: hsl(240, 100%, 70%);
+	}
+
+	mark > mark > mark > mark > mark > mark,
+	mark > mark > mark > mark > mark > mark:hover,
+	mark > mark > mark > mark > mark > mark:hover mark {
+		background: hsl(300, 100%, 65%);
 	}
 */});
 });
